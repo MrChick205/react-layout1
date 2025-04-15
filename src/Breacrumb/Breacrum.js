35bdeed2c6e2b85de19ab4f8dedd072d52b41dcb
MrChick205 <hvhiepbru@gmail.com>
2025-04-15 10:56:50 +0700

    first commit

---
+++ src/Breacrumb/Breacrum.js
@@ -0,0 +1,21 @@
+import React from 'react';
+class Breacrum extends React.Component {
+    render() {
+      return (
+        <div className="breacrumb-section">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="breadcrumb-text">
+                <a href="#"><i className="fa fa-home" /> Home</a>
+                <span>Shop</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> 
+      );
+    }
+  }
+  
+  export default Breacrum;
